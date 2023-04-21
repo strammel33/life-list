61efb04e0675b05ff4cf88ce8bcb83701036f073
Suzanne <trammelsuzanne@gmail.com>
2023-04-21 16:45:22 -0400

    make Record model

---
+++ models/record.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const recordSchema = new ({
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  date: {type: Date, required: true},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+}, {
+  timestamps: true
+})
+
+const Record = mongoose.model('Record', recordSchema)
+
+export {
+  Record
+}
