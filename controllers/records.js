6ee45b8c127af7152e66742cd53e38db1631e1d1
Suzanne <trammelsuzanne@gmail.com>
2023-04-22 12:25:28 -0400

    make new record controller

--- controllers/records.js
+++ controllers/records.js
@@ -1,7 +1,9 @@
 import { Record } from '../models/record.js'
 
 function newRecord(req, res) {
-  console.log('go to new page')
+  res.render('records/new', {
+    title: 'Add Record'
+  })
 }
 
 export {
