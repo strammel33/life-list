cf1470547ec01ffc2a6f39191a4ff0edcc300682
Suzanne <trammelsuzanne@gmail.com>
2023-04-23 12:43:21 -0400

    fix add bird bug to update existing birds

--- controllers/birds.js
+++ controllers/birds.js
@@ -2,33 +2,53 @@ import { Bird } from "../models/bird.js"
 import { Record } from "../models/record.js"
 
 function create(req, res) {
-  Bird.create(req.body)
-  .then (bird => {
-    Record.findById(req.params.recordId)
-    .populate('birds')
-    .then(record => {
-      bird.collectedDuring.push(record)
-      bird.save()
-      record.birds.push(bird)
-      record.save()
-      .then(birds => {
-        res.render('birds/add', {
-          birds,
-          record,
-          title: 'Add Birds'
+  let name = req.body.name
+  console.log(name)
+  let record = req.params.recordId
+  console.log(record)
+  Bird.findOne({name: name})
+  .then(bird=> {
+    if (bird) {
+      console.log('update bird')
+      Record.findById(record)
+        .populate('birds')
+        .then(record => {
+          bird.collectedDuring.push(record)
+          bird.save()
+          record.birds.push(bird)
+          record.save()
+          .then(birds => {
+            res.render('birds/add', {
+              birds,
+              record,
+              title: 'Add Birds'
+            })
+          })
+        })    
+    } else {
+      console.log('make new bird')
+      Bird.create(req.body)
+      .then(bird => {
+        Record.findById(record)
+        .populate('birds')
+        .then(record => {
+          bird.collectedDuring.push(record)
+          bird.save()
+          record.birds.push(bird)
+          record.save()
+          .then(birds => {
+            res.render('birds/add', {
+              birds,
+              record,
+              title: 'Add Birds'
+            })
+          })    
         })
-      })    
-      .catch(err => {
-      console.log(err)
-      res.redirect('/')
       })
-    })
-    .catch(err => {
-    console.log(err)
-    res.redirect('/')
-    })
+    }
   })
 }
+
 
 function index(req, res) {
   Bird.find({})
